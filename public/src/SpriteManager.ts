c6f9e9fbfa84cd6e012a116e1fed592280f9d039
Cezary Rutkowski <crutkow@gmail.com>
2026-06-09 21:07:59 +0200

    Fixed sprite coords and sizes

--- public/src/SpriteManager.ts
+++ public/src/SpriteManager.ts
@@ -16,27 +16,27 @@ export class SpriteManager {
 
   private initializeSprites(): void {
     // Brick colors: 64x32 each
-    this.sprites.set('brick-red', { name: 'brick-red', x: 0, y: 0, width: 64, height: 32 });
-    this.sprites.set('brick-orange', { name: 'brick-orange', x: 64, y: 0, width: 64, height: 32 });
-    this.sprites.set('brick-yellow', { name: 'brick-yellow', x: 128, y: 0, width: 64, height: 32 });
-    this.sprites.set('brick-green', { name: 'brick-green', x: 192, y: 0, width: 64, height: 32 });
-    this.sprites.set('brick-cyan', { name: 'brick-cyan', x: 256, y: 0, width: 64, height: 32 });
-    this.sprites.set('brick-blue', { name: 'brick-blue', x: 320, y: 0, width: 64, height: 32 });
-    this.sprites.set('brick-purple', { name: 'brick-purple', x: 384, y: 0, width: 64, height: 32 });
-    this.sprites.set('brick-magenta', { name: 'brick-magenta', x: 448, y: 0, width: 64, height: 32 });
+   this.sprites.set('brick-red', { name: 'brick-red', x: 43, y: 125, width: 118, height: 59 });
+    this.sprites.set('brick-orange', { name: 'brick-orange', x: 184, y: 125, width: 118, height: 59 });
+    this.sprites.set('brick-yellow', { name: 'brick-yellow', x: 326, y: 125, width: 118, height: 59 });
+    this.sprites.set('brick-green', { name: 'brick-green', x: 466, y: 125, width: 118, height: 59 });
+    this.sprites.set('brick-cyan', { name: 'brick-cyan', x: 43, y: 260, width: 118, height: 59 });
+    this.sprites.set('brick-blue', { name: 'brick-blue', x: 184, y: 260, width: 118, height: 59 });
+    this.sprites.set('brick-purple', { name: 'brick-purple', x: 326, y: 260, width: 118, height: 59 });
+    this.sprites.set('brick-magenta', { name: 'brick-magenta', x: 466, y: 260, width: 118, height: 59 });
 
-    // Paddles: 64x16 and 96x16
-    this.sprites.set('paddle-normal', { name: 'paddle-normal', x: 0, y: 32, width: 64, height: 16 });
-    this.sprites.set('paddle-long', { name: 'paddle-long', x: 64, y: 32, width: 96, height: 16 });
+    // Paddles: 172x48 and 276x48
+    this.sprites.set('paddle-normal', { name: 'paddle-normal', x: 867, y: 148, width: 172, height: 48 });
+    this.sprites.set('paddle-long', { name: 'paddle-long', x: 1084, y: 148, width: 276, height: 48 });
 
-    // Ball: 16x16
-    this.sprites.set('ball', { name: 'ball', x: 0, y: 48, width: 16, height: 16 });
+    // Ball: 42x42
+    this.sprites.set('ball', { name: 'ball', x: 882, y: 377, width: 42, height: 42 });
 
-    // Heart: 16x16
-    this.sprites.set('heart', { name: 'heart', x: 16, y: 48, width: 16, height: 16 });
+    // Heart: 35x35
+    this.sprites.set('heart', { name: 'heart', x: 1073, y: 481, width: 35, height: 35 });
 
-    // Speed arrow: 16x16
-    this.sprites.set('speed', { name: 'speed', x: 32, y: 48, width: 16, height: 16 });
+    // Speed arrow: 35x35
+    this.sprites.set('speed', { name: 'speed', x: 1205, y: 481, width: 35, height: 35 });
 
     // Multiplier 2x: 32x16
     this.sprites.set('mult-2x', { name: 'mult-2x', x: 48, y: 48, width: 32, height: 16 });
